711e40e0c471bfb1fffe3484801eb73d8e82a8f5
olegko <nobumori@mail.ru>
2016-07-23 15:20:18 +0300

    first step2

--- src/by/epamlab/Do.java
+++ src/by/epamlab/Do.java
@@ -3,8 +3,14 @@ package by.epamlab;
 
 public class Do {
 
-    public static void WriteMessage() {
-        System.out.println("Hello world");
+//    public static void WriteMessage() {
+//        System.out.println("Hello worlD");
+//    }
+
+    public void firstConstruct(){
+        String a = "HelloWorrld!";
+        String b = "WorldHello!";
+        System.out.println(a);
     }
 
 }
--- src/by/epamlab/Runner.java
+++ src/by/epamlab/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 
     public static void main(String[] args) {
         Do doing = new Do();
-        doing.WriteMessage();
+        doing.firstConstruct();
     }
 
 
