5a6216c07a572a77e5c44367f60bcc14248b1610
olegko <nobumori@mail.ru>
2016-07-23 14:48:44 +0300

    first step

---
+++ src/by/epamlab/Do.java
@@ -0,0 +1,10 @@
+package by.epamlab;
+
+
+public class Do {
+
+    public static void WriteMessage() {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/by/epamlab/Runner.java
@@ -0,0 +1,13 @@
+package by.epamlab;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        Do doing = new Do();
+        Do.WriteMessage();
+    }
+
+
+
+
+}
